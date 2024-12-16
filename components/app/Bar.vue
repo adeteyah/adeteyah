1907eb73f8e65d84eaaaf988676ba6a183cf2a74
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 16:56:10 +0000

    Dec 16, 2024, 11:56 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -5,6 +5,6 @@
       <NuxtLink class="dt-text-sm" to="/about">About</NuxtLink>
       <NuxtLink class="dt-text-sm" to="/work">Work</NuxtLink>
     </div>
-    <NuxtLink to="/contact">Contact</NuxtLink>
+    <NuxtLink class="dt-text-sm" to="/contact">Contact</NuxtLink>
   </nav>
 </template>
