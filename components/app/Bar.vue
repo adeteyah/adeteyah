db8b7fc2c403fc94eb91b5767a0647874fc563a9
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 15:51:07 +0000

    Dec 16, 2024, 10:51 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -1,10 +1,20 @@
 <template>
   <nav class="max-h-[54px]">
-    <NuxtLayout name="inline-flex">
-      <AppIcon />
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
+    <NuxtLayout name="inline-flex" class="justify-between w-full">
+      <NuxtLayout name="inline-flex">
+        <AppIcon />
+        <NuxtLink to="/">
+          <BaseTextSmall>Home</BaseTextSmall>
+        </NuxtLink>
+        <NuxtLink to="/about">
+          <BaseTextSmall>About</BaseTextSmall>
+        </NuxtLink>
+      </NuxtLayout>
+      <div>
+        <NuxtLink to="/contact">
+          <BaseTextSmall>Let's Start A Project</BaseTextSmall>
+        </NuxtLink>
+      </div>
     </NuxtLayout>
   </nav>
 </template>
