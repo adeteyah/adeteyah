e17d0e196e176774cbf39ebdd3bd73c45270c71f
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 16:21:50 +0000

    Dec 16, 2024, 11:21 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <NuxtLayout name="fullscreen">
     <NuxtLayout name="wrapper">
-      <AppBar />
+      <NuxtLayout name="wrapper" class="bg-slate-50">
+        <AppBar />
+      </NuxtLayout>
       <div class="grid grid-cols-2">
         <div>
           <NuxtLayout name="wrapper" class="bg-slate-50">
+            <BaseTextLarge>Nuxt Routing set up successfully!</BaseTextLarge>
             <BaseTextLarge>Nuxt Routing set up successfully!</BaseTextLarge>
           </NuxtLayout>
         </div>
