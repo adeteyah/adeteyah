6651dc0e1f1faee476ab16e33f249879f10538a2
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 17:12:07 +0000

    Dec 17, 2024, 12:12 AM

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="grid grid-cols-2">
-    <div class="grid grid-cols-2">
-      <div>Test</div>
-      <div>Test</div>
-    </div>
-    <div>Test</div>
+  <div class="grid grid-cols-4 grid-rows-3 gap-4 h-[100svh]">
+    <div class="col-span-2 row-span-2">1</div>
+    <div class="col-span-2 row-span-3 col-start-3">2</div>
+    <div class="col-span-2 row-start-3">3</div>
   </div>
 </template>
 <script setup lang="ts">
