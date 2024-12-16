896d82ec028e4ab15d3ceb331d1b412878424f72
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 16:26:10 +0000

    Dec 16, 2024, 11:26 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <NuxtLayout name="fullscreen">
-    <NuxtLayout name="wrapper">
-      <NuxtLayout name="wrapper" class="bg-slate-50">
+  <NuxtLayout name="fullscreen" class="bg-[purple]">
+    <NuxtLayout name="wrapper" class="h-full bg-[yellow]">
+      <NuxtLayout name="wrapper" class="bg-[red]">
         <AppBar />
       </NuxtLayout>
       <div class="grid grid-cols-2">
         <div>
-          <NuxtLayout name="wrapper" class="bg-slate-50">
+          <NuxtLayout name="wrapper" class="bg-slate-50 h-full">
             <BaseTextLarge>Nuxt Routing set up successfully!</BaseTextLarge>
             <BaseTextLarge>Nuxt Routing set up successfully!</BaseTextLarge>
           </NuxtLayout>
         </div>
-        <div>
+        <div class="h-full">
           <NuxtLayout name="wrapper"> Image </NuxtLayout>
         </div>
       </div>
