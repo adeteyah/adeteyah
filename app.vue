a628fb035ad080b2923abdb9d83859f3fa0dd5f2
Aditiya Rachman <adeteyah@gmail.com>
2024-12-16 10:34:26 +0000

    Dec 16, 2024, 5:34 PM

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@
   </template>
   <NuxtPage />
   <template>
-    <br /><br />
+    <br />
     <BaseTextHuge>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
