05d8db10d1c29a345239d909a8b36a5b859eb4bd
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 17:10:08 +0000

    Dec 17, 2024, 12:10 AM

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
-<template>test</template>
+<template>
+  <div class="grid grid-cols-2">
+    <div class="grid grid-cols-2">
+      <div>Test</div>
+      <div>Test</div>
+    </div>
+    <div>Test</div>
+  </div>
+</template>
 <script setup lang="ts">
 const route = useRoute();
 </script>
