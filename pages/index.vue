b09660ae70f54996cd49893dd21474c681d541c4
Dandi Darojat <adeteyah@gmail.com>
2024-12-16 16:13:15 +0000

    Dec 16, 2024, 11:13 PM

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,7 @@
     <NuxtLayout name="wrapper">
       <AppBar />
       <BaseTextLarge>Nuxt Routing set up successfully!</BaseTextLarge>
-      <p>Current route: {{ route.path }}</p>
-      <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-        >Learn more about Nuxt Routing</a
-      >
+      <div class=""></div>
     </NuxtLayout>
   </NuxtLayout>
 </template>
