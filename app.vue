fe9ceb72bef13c25ce7d3b2290fc7023a942bcac
Aditiya Rachman <adeteyah@gmail.com>
2024-12-16 08:06:39 +0700

    Init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <template>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  <NuxtPage />
+</template>
---
+++ components/app/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    class="w-12 md:w-24 lg:w-44"
+    viewBox="0 0 240 264"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_413_4245)">
+      <path d="M144 132H96L192 264H240L144 132Z" />
+      <path d="M96 0H48L144 132H192L96 0Z" />
+      <path d="M144 264H96L48 132V264H0V0H48L144 264Z" />
+    </g>
+    <defs>
+      <clipPath id="clip0_413_4245">
+        <rect width="240" height="264" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
