4a3a2e302c4909c094330e9891f7ab2d35067124
Aditiya Rachman <adeteyah@gmail.com>
2024-12-17 11:17:02 +0000

    Dec 17, 2024, 6:17 PM

--- components/app/TextLogo.vue
+++ components/app/TextLogo.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="group inline-block font-bold">
-    <span class="dt-transition-fade opacity-50 group-hover:opacity-100"
-      >Aditiya</span
-    >
     <span class="dt-transition-fade opacity-50 group-hover:opacity-100">
+      Aditiya
+    </span>
+    <span
+      class="dt-transition-fade opacity-50 group-hover:opacity-100 delay-150"
+    >
       Rachman
     </span>
   </div>
