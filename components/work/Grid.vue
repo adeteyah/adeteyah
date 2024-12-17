886b14b280de7700235adc4b4f3bb5316a36828c
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 08:34:03 +0000

    Dec 17, 2024, 3:34 PM

--- components/work/Grid.vue
+++ components/work/Grid.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div class="grid lg:grid-cols-6 dt-gap">
+    <div class="grid lg:grid-cols-6 dt-gap-gallery">
       <div v-for="work in workData" :key="work.id">
-        <nuxt-link class="group grid dt-gap" :to="`/work/${work.id}`">
+        <nuxt-link class="group grid gap-2" :to="`/work/${work.id}`">
           <div
             class="relative bg-lightgray lg:group-hover:bg-gray flex items-center aspect-video px-6 pt-8 lg:group-hover:pt-6 dt-transition-fade"
           >
@@ -12,15 +12,15 @@
               :alt="work.title"
             />
           </div>
-          <div class="grid dt-gap">
+          <div class="grid gap-1">
             <h3
-              class="text-2xl font-semibold lg:opacity-75 lg:group-hover:opacity-100 dt-transition-fade"
+              class="text-lg font-semibold lg:opacity-75 lg:group-hover:opacity-100 dt-transition-fade"
             >
               {{ work.title }}
             </h3>
-            <hr class="opacity-10" />
+            <UiDivider />
             <div
-              class="flex justify-between opacity-50 lg:group-hover:opacity-100 dt-transition-fade"
+              class="text-sm flex justify-between opacity-50 lg:group-hover:opacity-100 dt-transition-fade"
             >
               <p>{{ work.service }}</p>
               <p>{{ work.year }}</p>
