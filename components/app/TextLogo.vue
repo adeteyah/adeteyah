b2270aee86cbe21597f510a58283dbe8bc8b4f63
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 06:44:39 +0000

    Dec 17, 2024, 1:44 PM

--- components/app/TextLogo.vue
+++ components/app/TextLogo.vue
@@ -1,10 +1,10 @@
 <template>
-  <span class="group font-bold">
+  <div class="group font-bold">
     A<span class="dt-transition-fade opacity-0 group-hover:opacity-100"
       >ditiya</span
     >
     R<span class="dt-transition-fade opacity-0 group-hover:opacity-100"
       >achman
     </span>
-  </span>
+  </div>
 </template>
