4d98a2dbe8faccf75c35a7636da1fc41aa61dee4
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 08:17:21 +0000

    Dec 17, 2024, 3:17 PM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  app: {
-    pageTransition: {
-      name: "fade",
-    },
-  },
   modules: ["@nuxt/image", "@nuxt/fonts", "@nuxt/icon"],
   postcss: {
     plugins: {
