f44f60adf4fe3608acb50e91c9ac4d7fdede20be
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 05:53:10 +0000

    Dec 17, 2024, 12:53 PM

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,13 @@
     class="lg:grid grid-cols-4 grid-rows-3 dt-gap h-[100svh] dt-wrapper-base"
   >
     <div class="col-span-2 row-span-2">
-      <div class="bg-slate-50 h-full">Test</div>
+      <div class="bg-blue h-full">Test</div>
     </div>
     <div class="col-span-2 row-span-3 col-start-3">
-      <div class="bg-slate-50 h-full">Test</div>
+      <div class="bg-blue h-full">Test</div>
     </div>
     <div class="col-span-2 row-start-3">
-      <div class="bg-slate-50 h-full">Test</div>
+      <div class="bg-blue h-full">Test</div>
     </div>
   </div>
 </template>
-<script setup lang="ts">
-const route = useRoute();
-</script>
