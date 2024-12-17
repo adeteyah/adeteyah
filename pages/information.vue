f1f86fbd049226eadefa112eb27641deceb1f90f
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 09:11:44 +0000

    Dec 17, 2024, 4:11 PM

--- pages/information.vue
+++ pages/information.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>Information</div>
+  <div class="grid gap-4">
+    <p class="text-4xl leading-relaxed">
+      I am currently looking for work., Indonesia, working as a freelancer
+      globally since 2022. He enjoys collaborating with diverse teams and
+      clients, bringing innovative ideas to accessible websites through code. He
+      specializes in building websites with native JavaScript and WebGL for the
+      frontend, using Bun as both a runtime and backend.
+    </p>
+    <p class="text-4xl leading-relaxed">
+      For content management, he often works with WordPress and headless CMS
+      like Prismic or Sanity. Moreover, motion, deployment, and project
+      management are also part of his services. For any inquiries or
+      collaboration, whether you have a fully fleshed-out idea or just a spark
+      of inspiration, he’s excited to help bring your vision to life. Feel free
+      to reach out to him at hello@surya-aditya.com
+    </p>
+  </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
