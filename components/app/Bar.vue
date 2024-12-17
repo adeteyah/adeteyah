392d059f123be0ecbbc87964b9e478c9022d5ca8
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 07:38:03 +0000

    Dec 17, 2024, 2:38 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="dt-wrapper grid grid-flow-col dt-gap">
-    <div class="lg:hidden flex justify-between gap-2">
-      <NuxtLink class="text-sm dt-text-hover" to="/">Menu</NuxtLink>
+    <div class="lg:hidden flex justify-end items-center gap-2">
+      <NuxtLink class="text-sm dt-text-hover" to="/">
+        <Icon name="uil:bars" class="size-6" />
+      </NuxtLink>
     </div>
     <div class="hidden lg:flex flex-col gap-2">
       <NuxtLink class="text-sm dt-text-hover" to="/">Home</NuxtLink>
