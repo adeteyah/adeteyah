d4dda14ad0148683d5111d36ab1b12dcf800bc53
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 07:21:25 +0000

    Dec 17, 2024, 2:21 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -2,7 +2,7 @@
   <nav class="dt-wrapper grid grid-flow-col dt-gap">
     <div class="lg:hidden flex justify-between gap-2">
       <NuxtLink class="text-sm dt-text-hover" to="/">Menu</NuxtLink>
-      <Icon name="uil:github" />
+      <Icon name="uil:menu" />
     </div>
     <div class="hidden lg:flex flex-col gap-2">
       <h3>Menu</h3>
