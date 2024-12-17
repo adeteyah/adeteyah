3ac1e111769535829c80ae9fc0ccddb36db71b8c
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 08:58:08 +0000

    Dec 17, 2024, 3:58 PM

--- components/work/IndexGrid.vue
+++ components/work/IndexGrid.vue
@@ -1,30 +1,14 @@
 <template>
-  <div>
-    <div class="grid lg:grid-cols-2 dt-gap-gallery">
-      <div v-for="work in workData" :key="work.id">
+  <div class="flex bg-[red] h-full">
+    <div class="grid lg:grid-cols-4 dt-gap-gallery">
+      <div v-for="work in workData.slice(0, 4)" :key="work.id">
         <nuxt-link class="group grid gap-2" :to="`/work/${work.id}`">
-          <div
-            class="relative bg-lightgray lg:group-hover:bg-gray flex items-center aspect-video px-6 pt-8 lg:group-hover:pt-6 dt-transition-fade"
-          >
+          <div class="relative flex items-center aspect-video">
             <img
-              class="object-cover w-full h-full rounded-t-md"
+              class="object-cover w-full h-full rounded-md"
               :src="work.image"
               :alt="work.title"
             />
-          </div>
-          <div class="grid gap-1">
-            <h3
-              class="text-lg font-semibold lg:opacity-50 lg:group-hover:opacity-100 dt-transition-fade"
-            >
-              {{ work.title }}
-            </h3>
-            <UiDivider />
-            <div
-              class="text-sm flex justify-between opacity-25 lg:group-hover:opacity-100 dt-transition-fade"
-            >
-              <p>{{ work.service }}</p>
-              <p>{{ work.year }}</p>
-            </div>
           </div>
         </nuxt-link>
       </div>
