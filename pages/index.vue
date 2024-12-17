4fabc783d0ca7bf0a90c6e2aa88543ea892d2527
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 05:56:37 +0000

    Dec 17, 2024, 12:56 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
 <template>
-  <div
-    class="lg:grid grid-cols-4 grid-rows-3 dt-gap h-[100svh] dt-wrapper-base"
-  >
-    <div class="col-span-2 row-span-2">
-      <div class="bg-blue h-full">Test</div>
-    </div>
-    <div class="col-span-2 row-span-3 col-start-3">
-      <div class="bg-blue h-full">Test</div>
-    </div>
-    <div class="col-span-2 row-start-3">
-      <div class="bg-blue h-full">Test</div>
-    </div>
-  </div>
+  <AppBar />
 </template>
