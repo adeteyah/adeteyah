335da0cbc5be84e46d50bb626842db1385364486
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 07:25:43 +0000

    Dec 17, 2024, 2:25 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -2,34 +2,15 @@
   <nav class="dt-wrapper grid grid-flow-col dt-gap">
     <div class="lg:hidden flex justify-between gap-2">
       <NuxtLink class="text-sm dt-text-hover" to="/">Menu</NuxtLink>
-      <Icon name="uil:menu" />
     </div>
     <div class="hidden lg:flex flex-col gap-2">
-      <h3>Menu</h3>
       <NuxtLink class="text-sm dt-text-hover" to="/">Home</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">About</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Contact</NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/work">Information</NuxtLink>
     </div>
     <div class="hidden lg:flex flex-col gap-2">
-      <h3>Work</h3>
-      <NuxtLink class="text-sm dt-text-hover" to="/">Home</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Services</NuxtLink>
-    </div>
-    <div class="hidden lg:flex flex-col gap-2">
-      <h3>Awards / Recognition</h3>
-      <NuxtLink class="text-sm dt-text-hover" to="/">Home</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Services</NuxtLink>
-    </div>
-    <div class="hidden lg:flex flex-col gap-2">
-      <h3>Other Portfolio</h3>
-      <NuxtLink class="text-sm dt-text-hover" to="/">Home</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work">Services</NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/work">UI/UX Design</NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/">Website</NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/">Mobile App</NuxtLink>
     </div>
   </nav>
 </template>
