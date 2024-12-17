1d6e0c9773e7fdde37b20050d3873bbb8b19166e
Aditiya Rachman <adeteyah@gmail.com>
2024-12-17 11:08:46 +0000

    Dec 17, 2024, 6:08 PM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="dt-container flex gap-12">
-    <div class="h-svh flex flex-col justify-between">
+    <div class="w-6/12 lg:w-4/12 h-svh flex flex-col justify-between">
       <AppBar />
       <AppHeader />
       <AppIntro />
       <AppFooter />
     </div>
-    <div class="max-h-svh overflow-hidden overflow-y-auto">
+    <div class="w-6/12 lg:w-8/12 max-h-svh overflow-hidden overflow-y-auto">
       <main class="dt-wrapper">
         <slot />
       </main>
