d88bb2ad66369e3f1a1d47019ec9e4753ebf0c8a
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 08:26:21 +0000

    Dec 17, 2024, 3:26 PM

--- components/work/Grid.vue
+++ components/work/Grid.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="grid lg:grid-cols-2 gap-y-16 gap-x-8">
+    <div class="grid lg:grid-cols-4 dt-gap">
       <div
-        v-for="work in shouldSlice ? workData.slice(0, 2) : workData"
+        v-for="work in shouldSlice ? workData.slice(0, 4) : workData"
         :key="work.id"
       >
-        <nuxt-link class="group grid gap-8" :to="`/work/${work.id}`">
+        <nuxt-link class="group grid dt-gap" :to="`/work/${work.id}`">
           <div
-            class="relative bg-gray lg:group-hover:bg-gray/25 flex items-center transition-all duration-500 aspect-video px-4 pt-8 lg:group-hover:pt-4"
+            class="relative bg-lightgray lg:group-hover:bg-gray flex items-center aspect-video px-6 pt-8 lg:group-hover:pt-6 dt-transition-fade"
           >
             <img
               class="object-cover w-full h-full rounded-t-md"
@@ -15,15 +15,15 @@
               :alt="work.title"
             />
           </div>
-          <div class="grid gap-4">
+          <div class="grid dt-gap">
             <h3
-              class="text-3xl font-semibold lg:opacity-75 lg:group-hover:opacity-100 transition-all duration-500"
+              class="text-2xl font-semibold lg:opacity-75 lg:group-hover:opacity-100 dt-transition-fade"
             >
               {{ work.title }}
             </h3>
             <hr class="opacity-10" />
             <div
-              class="flex justify-between opacity-50 lg:group-hover:opacity-100 transition-all duration-500"
+              class="flex justify-between opacity-50 lg:group-hover:opacity-100 dt-transition-fade"
             >
               <p>{{ work.service }}</p>
               <p>{{ work.year }}</p>
