38f288305aa63cd84ecab0c79132ceca0c6147ed
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 09:06:15 +0000

    Dec 17, 2024, 4:06 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <WorkIndexGrid />
+  <p class="text-4xl">
+    Surya Aditya is a website developer based in Bali, Indonesia, working as a
+    freelancer globally since 2022. He enjoys collaborating with diverse teams
+    and clients, bringing innovative ideas to accessible websites through code.
+    He specializes in building websites with native JavaScript and WebGL for the
+    frontend, using Bun as both a runtime and backend. For content management,
+    he often works with WordPress and headless CMS like Prismic or Sanity.
+    Moreover, motion, deployment, and project management are also part of his
+    services. For any inquiries or collaboration, whether you have a fully
+    fleshed-out idea or just a spark of inspiration, he’s excited to help bring
+    your vision to life. Feel free to reach out to him at hello@surya-aditya.com
+  </p>
 </template>
