fe095a58403ba7d97321fbc4b8b15870b760e7f1
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 06:59:24 +0000

    Dec 17, 2024, 1:59 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="dt-wrapper flex justify-between dt-gap">
     <NuxtLink class="dt-text-sm" to="/">Home</NuxtLink>
-    <NuxtLink class="dt-text-sm" to="/about">About</NuxtLink>
     <NuxtLink class="dt-text-sm" to="/work">Work</NuxtLink>
+    <NuxtLink class="dt-text-sm" to="/about">About</NuxtLink>
   </nav>
 </template>
