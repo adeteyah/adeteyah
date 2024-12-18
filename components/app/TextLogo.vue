21bc8700f3374f0b84afa77c3068938d2555ee4d
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 07:53:50 +0000

    Dec 18, 2024, 2:53 PM

--- components/app/TextLogo.vue
+++ components/app/TextLogo.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="inline-block font-bold">
+  <div class="inline font-bold">
     <span class="dt-transition-fade opacity-50 group-hover:opacity-100">
       Aditiya
     </span>
     <span
-      class="dt-transition-fade opacity-50 group-hover:opacity-100 delay-150"
+      class="dt-transition-fade opacity-50 group-hover:opacity-100 delay-200"
     >
       Rachman
     </span>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="default" class="flex">
-    <div class="w-4/12 lg:w-4/12 flex flex-col justify-between group pl-8 pr-4">
+    <div class="w-1/3 lg:w-4/12 flex flex-col justify-between group pl-8 pr-4">
       <AppBar />
       <AppHeader />
       <AppFooter />
     </div>
-    <div class="w-8/12 lg:w-8/12 pr-8 pl-4">
+    <div class="w-2/3 lg:w-8/12 pr-8 pl-4">
       <main class="dt-wrapper">
         <slot />
       </main>
