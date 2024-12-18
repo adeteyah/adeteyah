ae4dfe5e0034b86bc138f845ec28a13601e2f745
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 04:53:42 +0000

    Dec 18, 2024, 11:53 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="flex">
-    <div
-      class="w-4/12 lg:w-4/12 h-svh flex flex-col justify-between group px-8"
-    >
+  <div id="default" class="flex">
+    <div class="w-4/12 lg:w-4/12 h-svh flex flex-col justify-between group">
       <AppBar />
       <AppHeader />
       <AppIntro />
       <AppFooter />
     </div>
-    <div
-      class="w-7/12 lg:w-8/12 max-h-svh overflow-hidden overflow-y-auto px-8"
-    >
+    <div class="w-8/12 lg:w-8/12 max-h-svh overflow-hidden overflow-y-auto">
       <main class="dt-wrapper">
         <slot />
       </main>
     </div>
   </div>
 </template>
+<style scoped>
+#default > div {
+  @apply px-4 lg:px-8;
+}
+</style>
