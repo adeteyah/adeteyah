b18085f202887fde21f6750c2de8641d4017d6b8
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 22:17:05 +0000

    Dec 19, 2024, 5:17 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
     class="flex transition-colors duration-1000 delay-500"
     :class="{ 'bg-lightgray': $route.path.startsWith('/information') }"
   >
-    <div class="group/left w-1/3 lg:w-4/12 pl-6 lg:pl-12 pr-2 lg:pr-4">
+    <div class="group/a w-1/3 lg:w-4/12 pl-6 lg:pl-12 pr-2 lg:pr-4">
       <div
-        class="flex flex-col h-full lg:opacity-50 lg:group-hover/left:opacity-100 dt-transition-opacity"
+        class="flex flex-col h-full lg:opacity-50 lg:group-hover/a:opacity-100 dt-transition-opacity"
       >
         <div class="flex-1">
           <AppBar />
@@ -19,9 +19,9 @@
         </div>
       </div>
     </div>
-    <div class="group/right w-2/3 lg:w-8/12 pr-6 lg:pr-12 pl-2 lg:pl-4">
+    <div class="group/a w-2/3 lg:w-8/12 pr-6 lg:pr-12 pl-2 lg:pl-4">
       <div
-        class="lg:opacity-50 lg:group-hover/right:opacity-100 dt-transition-opacity dt-wrapper"
+        class="lg:opacity-50 lg:group-hover/a:opacity-100 dt-transition-opacity dt-wrapper"
       >
         <slot />
       </div>
