f2c87e471fc3db48015dc501aa1aa97a158bb088
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 12:03:55 +0000

    Dec 18, 2024, 7:03 PM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white dt-wrapper px-4 md:px-6 lg:px-12 rounded-md grid dt-gap">
-    <p class="text-9xl">Helping brand s thrive in the digital world</p>
+    <p class="text-8xl">Helping brand s thrive in the digital world</p>
     <p>
       I help companies from all over the world with tailor-made solutions. With
       each project, I push my work to new horizons, always putting quality
