b033af985c08831a0bb867dde33a0de4e10a7eb3
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 08:08:24 +0000

    Dec 18, 2024, 3:08 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -1,8 +1,17 @@
 <template>
   <nav class="hidden dt-wrapper lg:flex dt-gap">
     <div class="flex flex-1 flex-col gap-2 items-start">
-      <NuxtLink class="text-sm dt-text-hover" to="/">Work</NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/information">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/' }"
+        to="/"
+        >Work</NuxtLink
+      >
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/information' }"
+        to="/information"
+      >
         Information
       </NuxtLink>
     </div>
@@ -10,27 +19,51 @@
       class="flex flex-1 flex-col gap-2 items-start"
       v-if="/^\/$|^\/work\//.test($route.path)"
     >
-      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/uiux">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/work/tag/uiux' }"
+        to="/work/tag/uiux"
+      >
         UI/UX Design
       </NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/website">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/work/tag/website' }"
+        to="/work/tag/website"
+      >
         Website
       </NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/mobile">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/work/tag/mobile' }"
+        to="/work/tag/mobile"
+      >
         Mobile App
       </NuxtLink>
     </div>
     <div
       class="flex flex-1 flex-col gap-2 items-start"
-      v-if="$route.path == '/information'"
+      v-if="/^\/$|^\/information\//.test($route.path)"
     >
-      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/uiux">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/information' }"
+        to="/information/whatelse"
+      >
         UI/UX Desisssgn
       </NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/website">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/information' }"
+        to="/information/whatelse"
+      >
         Website
       </NuxtLink>
-      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/mobile">
+      <NuxtLink
+        class="text-sm dt-text-hover"
+        :class="{ 'font-bold': $route.path === '/information' }"
+        to="/information/whatelse"
+      >
         Mobile App
       </NuxtLink>
     </div>
