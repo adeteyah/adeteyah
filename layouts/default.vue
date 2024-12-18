e9008386d00f468d0fcc1c573adf9b0fa68e56b5
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 06:40:41 +0000

    Dec 18, 2024, 1:40 PM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="default" class="flex">
-    <div class="w-4/12 lg:w-4/12 flex flex-col justify-between group">
+  <div id="default" class="flex gap-8">
+    <div class="w-4/12 lg:w-4/12 flex flex-col justify-between group pl-8">
       <AppBar />
       <AppHeader />
       <AppIntro />
       <AppFooter />
     </div>
-    <div class="w-8/12 lg:w-8/12">
+    <div class="w-8/12 lg:w-8/12 pr-8">
       <main class="dt-wrapper">
         <slot />
       </main>
