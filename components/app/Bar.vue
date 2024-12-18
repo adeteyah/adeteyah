d00ef3f0f4c1965080debc647a53be2b973d1750
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 07:21:11 +0000

    Dec 18, 2024, 2:21 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -1,12 +1,23 @@
 <template>
   <nav class="hidden dt-wrapper lg:grid grid-flow-col dt-gap">
     <div class="flex flex-col gap-2 items-start">
-      <NuxtLink class="text-sm dt-text-hover" to="/">Home</NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/">Work</NuxtLink>
       <NuxtLink class="text-sm dt-text-hover" to="/information">
         Information
       </NuxtLink>
     </div>
-    <div class="flex flex-col gap-2 items-start">
+    <div id="work-option" class="flex flex-col gap-2 items-start">
+      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/uiux">
+        UI/UX Design
+      </NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/website">
+        Website
+      </NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/mobile">
+        Mobile App
+      </NuxtLink>
+    </div>
+    <div id="information-option" class="flex flex-col gap-2 items-start">
       <NuxtLink class="text-sm dt-text-hover" to="/work/tag/uiux">
         UI/UX Design
       </NuxtLink>
