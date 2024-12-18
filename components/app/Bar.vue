15c59528797866e8f795e0378bc8fbbad8548028
Aditiya Rachman <adeteyah@gmail.com>
2024-12-18 07:42:39 +0000

    Dec 18, 2024, 2:42 PM

--- components/app/Bar.vue
+++ components/app/Bar.vue
@@ -1,14 +1,31 @@
 <template>
-  <nav class="hidden dt-wrapper lg:grid grid-flow-col dt-gap">
-    <div class="flex flex-col gap-2 items-start">
+  <nav class="hidden dt-wrapper lg:flex dt-gap">
+    <div class="flex flex-1 flex-col gap-2 items-start">
       <NuxtLink class="text-sm dt-text-hover" to="/">Work</NuxtLink>
       <NuxtLink class="text-sm dt-text-hover" to="/information">
         Information
       </NuxtLink>
     </div>
-    <div class="flex flex-col gap-2 items-start">
+    <div
+      class="flex flex-1 flex-col gap-2 items-start"
+      v-if="$route.path == '/'"
+    >
       <NuxtLink class="text-sm dt-text-hover" to="/work/tag/uiux">
         UI/UX Design
+      </NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/website">
+        Website
+      </NuxtLink>
+      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/mobile">
+        Mobile App
+      </NuxtLink>
+    </div>
+    <div
+      class="flex flex-1 flex-col gap-2 items-start"
+      v-if="$route.path == '/information'"
+    >
+      <NuxtLink class="text-sm dt-text-hover" to="/work/tag/uiux">
+        UI/UX Desisssgn
       </NuxtLink>
       <NuxtLink class="text-sm dt-text-hover" to="/work/tag/website">
         Website
