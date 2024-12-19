24ebe6133a83904f101f550c46da9f72ab166c9a
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 04:40:47 +0000

    Dec 19, 2024, 11:40 AM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>About me</div>
-  <div>Skill</div>
-  <div>Experience</div>
-  <div>Testimonials</div>
-  <div>Contact</div>
+  <div class="dt-bg-invert p-4">
+    <div>About me</div>
+    <div>Skill</div>
+    <div>Experience</div>
+    <div>Testimonials</div>
+    <div>Contact</div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
