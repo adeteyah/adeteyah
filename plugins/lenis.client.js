26a6e9ccb55a115d2cba871dbe5023550a3302a6
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 03:18:39 +0000

    Dec 19, 2024, 10:18 AM

--- plugins/lenis.client.js
+++ plugins/lenis.client.js
@@ -1,23 +1,18 @@
 import Lenis from "lenis";
-
 export default defineNuxtPlugin((nuxtApp) => {
-  const scrollContainer = document.querySelector("#scroll-container"); // Target your element here
-
+  const scrollContainer = document.querySelector("#scroll-container");
   if (scrollContainer) {
     const lenis = new Lenis({
       wrapper: scrollContainer,
-      content: scrollContainer.firstElementChild, // Target the scrollable content
-      lerp: 0.1, // Adjust smoothness
-      smooth: true, // Enable smooth scrolling
+      content: scrollContainer.firstElementChild,
+      lerp: 0.1,
+      smooth: true,
     });
-
     function raf(time) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
-
     nuxtApp.provide("lenis", lenis);
   }
 });
