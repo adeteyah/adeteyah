3d6d6d24fa77414efec5f7092ca31fcc5b853655
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 12:10:51 +0000

    Dec 19, 2024, 7:10 PM

--- pages/work/[slug].vue
+++ pages/work/[slug].vue
@@ -1,5 +1,33 @@
 <template>
-  <div>Test</div>
+  <div class="grid gap-12">
+    <div class="grid lg:grid-cols-2">
+      <div class="grid gap-4">
+        <img
+          class="object-cover w-full h-full"
+          src="https://dennissnellenberg.com/assets/img/DSC07312-2.jpg"
+        />
+        <img
+          class="object-cover w-full h-full"
+          src="https://dennissnellenberg.com/assets/img/DSC07312-2.jpg"
+        />
+        <img
+          class="object-cover w-full h-full"
+          src="https://dennissnellenberg.com/assets/img/DSC07312-2.jpg"
+        />
+      </div>
+      <div class="text-2xl leading-9 lg:p-24">
+        <div class="block mb-12">
+          <Icon name="rivet-icons:arrow-down-right" size="42" />
+        </div>
+        With a background in design, I work closely with design focused teams to
+        build websites and microsites for companies and individuals. I have
+        years of experience working and collaborating on product teams and
+        leading engineering efforts. With my experience in UI and product
+        engineering, I solve product problems and build appealing usable web
+        experiences.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
