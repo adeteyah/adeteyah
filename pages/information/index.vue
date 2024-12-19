b9e1efc91b03ccdba0942a5e8726e6e543b8e8ec
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 08:54:31 +0000

    Dec 19, 2024, 3:54 PM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="grid dt-gap">
+  <div
+    class="grid dt-gap text-xl leading-relaxed md:text-2xl md:leading-relaxed lg:text-3xl lg:leading-relaxed"
+  >
     With a background in design, I work closely with design focused teams to
     build websites and microsites for companies and individuals. I have years of
     experience working and collaborating on product teams and leading
