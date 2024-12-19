a53f2e09607dc86c10456356136fa66248066d38
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 11:49:08 +0000

    Dec 19, 2024, 6:49 PM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -55,6 +55,26 @@
         </AboutRecognitionList>
       </div>
     </div>
+    <div class="grid lg:grid-cols-2">
+      <div class="text-2xl leading-9 lg:p-24">
+        <div class="block mb-12">
+          <Icon name="rivet-icons:arrow-right" size="42" />
+        </div>
+        <p
+          class="text-xl leading-relaxed md:text-3xl md:leading-relaxed lg:text-6xl lg:leading-relaxed"
+        >
+          Contact
+        </p>
+        <p
+          class="text-lg leading-relaxed md:text-xl md:leading-relaxed lg:text-2xl lg:leading-relaxed"
+        >
+          Let's start a project together
+        </p>
+      </div>
+      <div class="bg-black/5 dark:bg-white/5 p-12">
+        I'm always happy to talk. AVAILABLE FOR FREELANCE WORK
+      </div>
+    </div>
   </div>
 </template>
 
