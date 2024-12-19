f59407d7e27b7b68dce7c553757a25d521472da3
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 10:50:02 +0000

    Dec 19, 2024, 5:50 PM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -2,6 +2,9 @@
   <div class="grid gap-12">
     <div class="grid lg:grid-cols-2">
       <div class="text-2xl leading-9 p-24">
+        <div class="block">
+          <Icon name="lucide:arrow-down-right" size="96" />
+        </div>
         With a background in design, I work closely with design focused teams to
         build websites and microsites for companies and individuals. I have
         years of experience working and collaborating on product teams and
