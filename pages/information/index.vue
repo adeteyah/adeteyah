a5e197e226f847747462e90ea8044198aca9eae8
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 04:56:10 +0000

    Dec 19, 2024, 11:56 AM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -1,31 +1,13 @@
 <template>
-  <div>Skill</div>
-  <div class="grid grid-cols-6 gap-4">
-    <div class="bg-black rounded p-4">
-      <div class="">HTML/CSS</div>
-    </div>
-    <div class="bg-black rounded p-4">
-      <div class="">JS/TS</div>
-    </div>
-    <div class="bg-black rounded p-4">
-      <div class="">Laravel</div>
-    </div>
-    <div class="bg-black rounded p-4">
-      <div class="">Nuxt</div>
-    </div>
-    <div class="bg-black rounded p-4">
-      <div class="">Flutter</div>
-    </div>
-    <div class="bg-black rounded p-4">
-      <div class="">Vue</div>
-    </div>
-    <div class="bg-black rounded p-4">
-      <div class="">Laravel</div>
-    </div>
+  <div class="grid grid-cols-8 gap-4">
+    <div class="">HTML/CSS</div>
+    <div class="">JS/TS</div>
+    <div class="">Laravel</div>
+    <div class="">Nuxt</div>
+    <div class="">Flutter</div>
+    <div class="">Vue</div>
+    <div class="">Laravel</div>
   </div>
-  <div>Experience</div>
-  <div>Testimonials</div>
-  <div>Contact</div>
 </template>
 
 <script lang="ts" setup></script>
