1d57997ce5ff982587151c7ceb902d565b46e0c2
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 10:00:04 +0000

    Dec 19, 2024, 5:00 PM

--- pages/information/index.vue
+++ pages/information/index.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="grid grid-cols-2 dt-gap">
-    <div
-      class="grid dt-gap text-xl leading-relaxed md:text-2xl md:leading-relaxed lg:text-3xl lg:leading-relaxed"
-    >
-      <div class="p-8 group-hover/card:pt-5 bg-black/10 dark:bg-white/10">
-        With a background in design, I work closely with design focused teams to
-        build websites and microsites for companies and individuals. I have
-        years of experience working and collaborating on product teams and
-        leading engineering efforts. With my experience in UI and product
-        engineering, I solve product problems and build appealing usable web
-        experiences.
-      </div>
+  <div class="grid lg:grid-cols-2 gap-12">
+    <div class="text-2xl leading-9">
+      With a background in design, I work closely with design focused teams to
+      build websites and microsites for companies and individuals. I have years
+      of experience working and collaborating on product teams and leading
+      engineering efforts. With my experience in UI and product engineering, I
+      solve product problems and build appealing usable web experiences.
     </div>
-    <div class="p-8 group-hover/card:pt-5 bg-black/10 dark:bg-white/10">
-      <img
-        class="object-cover w-full h-full"
-        src="https://dennissnellenberg.com/assets/img/DSC07312-2.jpg"
-      />
-    </div>
+    <img
+      class="object-cover w-full h-full"
+      src="https://dennissnellenberg.com/assets/img/DSC07312-2.jpg"
+    />
   </div>
 </template>
 
