60c00065e20fe535296577a96ef6a57b6650255d
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 11:22:51 +0000

    Dec 19, 2024, 6:22 PM

--- pages/information/contact.vue
+++ pages/information/contact.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="grid gap-12">
     <div class="grid grid-cols-2">
-      <p
-        class="text-2xl leading-relaxed md:text-4xl md:leading-relaxed lg:text-8xl lg:leading-relaxed"
-      >
-        Let's start a project together
-      </p>
+      <div>
+        <p
+          class="text-2xl leading-relaxed md:text-4xl md:leading-relaxed lg:text-8xl lg:leading-relaxed"
+        >
+          Contact
+        </p>
+        <p
+          class="text-xl leading-relaxed md:text-2xl md:leading-relaxed lg:text-4xl lg:leading-relaxed"
+        >
+          Let's start a project together
+        </p>
+      </div>
       <p
         class="text-2xl leading-relaxed md:text-4xl md:leading-relaxed lg:text-8xl lg:leading-relaxed"
       >
