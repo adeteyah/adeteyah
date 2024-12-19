e966f66bb2cf576f846f44935decc6ec56b20b2a
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 04:43:28 +0000

    Dec 19, 2024, 11:43 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     id="default"
     class="flex transition-colors duration-1000 delay-500"
     :class="{
-      'bg-[#808080] dark:bg-[#242424]': $route.path.startsWith('/information'),
+      'dt-invert-bg': $route.path.startsWith('/information'),
     }"
   >
     <div class="w-1/3 lg:w-4/12 pl-6 lg:pl-12 pr-2 lg:pr-4">
