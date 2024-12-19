96f8701297534d7c68d10a0eab6cc96b6b6df94b
Dandi Darojat <adeteyah@gmail.com>
2024-12-19 11:00:10 +0000

    Dec 19, 2024, 6:00 PM

--- pages/information/contact.vue
+++ pages/information/contact.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-    
+  <div class="grid gap-12">
+    <div class="">
+      <p class="text-9xl leading-relaxed p-24 uppercase">
+        Let's Talk About The Project?
+      </p>
+    </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
